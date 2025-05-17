84a5ff27c9a49c8b5182c97cb035a4199bd84c7f
oikumo <oikumo@gmail.com>
2025-05-17 18:24:13 -0400

    Fixed

--- src/runner/test-runner-results.ts
+++ src/runner/test-runner-results.ts
@@ -1,16 +1,19 @@
 import { TestResult } from './test-result';
 
 export class TestRunnerResults {
-    results: Array<TestResult>;
-    passed: Array<TestResult>;
-    failed: Array<TestResult>;
-    runnerErrors: Array<TestResult>;
+    results = new Array<TestResult>();
+    passed = new Array<TestResult>();
+    failed = new Array<TestResult>();
+    runnerErrors = new Array<TestResult>();
 
     constructor(results: Array<TestResult>) {
-        this.results = new Array<TestResult>();
+        this.results = results;
+        
+        /*new Array<TestResult>();
         for (let i = 0; i < results.length; i++) {
             this.results.push(results[i].clone());
         }
+        */
 
         this.passed = this.results.filter((result) => result.errors.length === 0);
         this.failed = this.results.filter((result) => result.errors.length > 0);
